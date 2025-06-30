2809ffd5e0acabeb522b8177ff880d83f45dca88
HJJ <goldmonkey33@naver.com>
2025-07-01 03:29:48 +0900

    add composable function MessageCard

--- app/src/main/java/com/example/madcampproj1/MainActivity.kt
+++ app/src/main/java/com/example/madcampproj1/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.madcampproj1.ui.theme.MadCampProj1Theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,4 +54,35 @@ fun GreetingPreview() {
     MadCampProj1Theme {
         Greeting("Android")
     }
+}
+
+data class Message(val auth: String, val body: String)
+
+// Column, Row, Box 를 사용할 수 있음
+@Composable
+fun MessageCard(mSg: Message) {
+Row {
+    Image(
+        painter = painterResource(id=R.drawable.my_image),
+        contentDescription = "profile image",
+        modifier = Modifier
+            .size(120.dp),  // 가로, 세로 모두 120dp로 설정
+        contentScale = ContentScale.Fit,  // 잘리지 않고 맞게 축소됨
+    )
+    
+    // image 와 작성자 정보 사이에 공란 추가
+    Spacer(modifier = Modifier.width(8.dp))
+
+    Column {
+        Text(text = mSg.auth)
+        Text(text = mSg.body)
+    }
+}
+
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(mSg = Message("someone", "My First Jetpack Compose"))
 }
